91bb6b88a16e5940ba9fcdf80b138c6d3434da31
Lara <brusel@usi.ch>
2017-12-04 16:49:27 +0100

    terrain with perlin noise

--- rocket_corgi_src/terrain.h
+++ rocket_corgi_src/terrain.h
@@ -19,9 +19,21 @@ void drawTerrain()
 
     float cur_x,z;
     float x[2]; //keeps last and next x
-
-    glPolygonMode(GL_FRONT_AND_BACK ,GL_SMOOTH);
+    glDisable(GL_LIGHT0);
+    GLfloat light_ambient[] = {1.0, 1.0, 0.0, 1.0};
+    GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+    GLfloat light_specular[] = {1.0, 1.0, 1.0, 1.0};
+    GLfloat light_position[] = {0, 0, 1, 0.0};
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    //glColor3f(0.0, 0.0, 1.0);
+    glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
     glShadeModel(GL_SMOOTH);
+    //glEnable ( GL_COLOR_MATERIAL );
+    glEnable(GL_LIGHT0);
 
     // 1000 strips, 1000 triangles long
     x[0] = -world_dim/2.0;
@@ -34,17 +46,18 @@ void drawTerrain()
         glBegin(GL_TRIANGLE_STRIP);
             z=0.0;
             Point3d off = Point3d(1.0, 1.0, 0.0);
-            siv::PerlinNoise perlinNoise(42);
+            siv::PerlinNoise perlin(12345);
             for(int j=0; j<world_dim; j++)
             {
-                Point3d p = Point3d(cur_x, noise(cur_x, z) , z + 100);
+                Point3d p = Point3d(cur_x, perlin.octaveNoise(cur_x  * 0.1, -z  * 0.1, 8) * 2 , z + 100);
                 Point2d p_xy = Point2d(p.x(), p.y());
                 Point2d off_xz = Point2d(off.x(), off.z());
                 Point2d off_zy = Point2d(off.z(), off.y());
-                float hL = noise((p_xy - off_xz).x(), (p_xy - off_xz).y());
-                float hR = noise((p_xy + off_xz).x(), (p_xy + off_xz).y());
-                float hD = noise((p_xy - off_zy).x(), (p_xy - off_zy).y());
-                float hU = noise((p_xy + off_zy).x(), (p_xy + off_zy).y());
+                float hL = perlin.octaveNoise((p_xy - off_xz).x() * 0.1, (p_xy - off_xz).y()  * 0.1, 8);
+
+                float hR = perlin.octaveNoise((p_xy + off_xz).x() * 0.1, (p_xy + off_xz).y() * 0.1, 8);
+                float hD = perlin.octaveNoise((p_xy - off_zy).x() * 0.1, (p_xy - off_zy).y() * 0.1, 8 );
+                float hU = perlin.octaveNoise((p_xy + off_zy).x() * 0.1, (p_xy + off_zy).y() * 0.1, 8);
 
                 // deduce terrain normal
                 Point3d n = Point3d(hL - hR, hD - hU, 2.0).normalized();
